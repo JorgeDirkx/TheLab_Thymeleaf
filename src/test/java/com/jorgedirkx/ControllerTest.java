41bcbd1e13c21530d020f3e44f8a531a5cdb8ff9
JorgeDirkx <jorge.dirkx@gmail.com>
2020-09-13 20:41:15 +0200

    Testing to add

---
+++ src/test/java/com/jorgedirkx/ControllerTest.java
@@ -0,0 +1,26 @@
+package com.jorgedirkx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.StatusAssertions;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = ControllerTest.class)
+public class ControllerTest {
+
+    //to add
+}
+
+
